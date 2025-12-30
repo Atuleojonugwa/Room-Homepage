cbdc829400f0cadcc174442d9d7cecae97280678
Atuleojonugwa <ojonugwaatule@gmail.com>
2025-12-30 20:13:01 +0100

    responsiveness for 2xl screens

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -14,13 +14,13 @@ function About() {
           />
         </div>
 
-        <div className="md:w-[40%] w-full flex flex-col justify-center p-8 md:p-12 ">
+        <div className="md:w-[40%] w-full flex flex-col justify-center p-8 md:p-12 2xl:text-center">
           {/* <div className="max-w-md mx-auto"> */}
-            <h2 className="text-black font-bold uppercase tracking-[0.4em] mb-4 text-sm">
+            <h2 className="text-black font-bold uppercase tracking-[0.4em] mb-4 text-sm 2xl:text-center 2xl:text-2xl">
               about our furniture
             </h2>
 
-            <p className="text-gray-500 text-[12px] leading-5 font-medium">
+            <p className="text-gray-500 text-[12px] leading-5 font-medium 2xl:text-lg 2xl:leading-8">
               Our multifunctional collection blends design and function to suit
               your <br /> individual taste. Make each room unique, or pick a
               cohesive theme that best <br /> express your interests and what
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 import hero1 from "../assets/desktop-image-hero-1.jpg";
 import hero2 from "../assets/desktop-image-hero-2.jpg";
 import hero3 from "../assets/desktop-image-hero-3.jpg";
-import closeIcon from "../assets/icon-close.svg"
-import hamburgerIcon from "../assets/icon-hamburger.svg"
+import closeIcon from "../assets/icon-close.svg";
+import hamburgerIcon from "../assets/icon-hamburger.svg";
 import logo from "../assets/logo.svg";
 
 const SliderButton = ({ children, onClick }) => (
   <button
     onClick={onClick}
-    className="bg-black w-20 h-20 flex items-center justify-center cursor-pointer hover:bg-(--grey-800) transition-colors focus:outline-none"
+    className="bg-black w-12 h-12 lg:w-20 lg:h-20 2xl:w-28 2xl:h-28 flex items-center justify-center cursor-pointer hover:bg-gray-800 transition-colors focus:outline-none"
   >
     {children}
   </button>
@@ -20,26 +20,36 @@ const Iconarrow = () => (
   <svg
     width="40"
     height="12"
-    className="fill-current"
+    className="fill-current w-10 h-3 2xl:w-16 2xl:h-5"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z"
       fill="currentColor"
-      fill-rule="nonzero"
+      fillRule="nonzero"
     />
   </svg>
 );
 
 const Angleright = () => (
-  <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd" />
+  <svg
+    width="14"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    className="2xl:scale-150"
+  >
+    <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd" />
   </svg>
 );
 
 const Angleleft = () => (
-  <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg">
-    <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd" />
+  <svg
+    width="14"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    className="2xl:scale-150"
+  >
+    <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd" />
   </svg>
 );
 
@@ -64,7 +74,6 @@ const Slides = [
   },
 ];
 
-
 function Homepage() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [openMenu, setOpenMenu] = useState(false);
@@ -72,17 +81,13 @@ function Homepage() {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
-        setOpenMenu(false); // Close mobile menu automatically on desktop
+        setOpenMenu(false);
       }
     };
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-
-
-
 
   const nextSlide = () => {
     setCurrentSlide((prev) => (prev === Slides.length - 1 ? 0 : prev + 1));
@@ -92,110 +97,100 @@ function Homepage() {
     setCurrentSlide((prev) => (prev === 0 ? Slides.length - 1 : prev - 1));
   };
 
-// 
-
   const activeSlide = Slides[currentSlide];
   return (
-    <div className="flex flex-col md:flex-row w-full md:h-150 ">
-
+    <div className="flex flex-col md:flex-row w-full md:h-150 2xl:h-200">
       <div className="relative h-full md:static w-full md:w-[72%]">
         <div className="h-full">
-          <img src={activeSlide.Image} alt="hero-Image" className="w-full h-full object-cover block" />
-          <div className="
-  flex
-  absolute bottom-0 right-0
- md:hidden
-">
+          <img
+            src={activeSlide.Image}
+            alt="hero-Image"
+            className="w-full h-full object-cover block"
+          />
 
+          <div className="flex absolute bottom-0 right-0 md:hidden">
             <SliderButton onClick={prevSlide}>
               <Angleleft />
             </SliderButton>
-
             <SliderButton onClick={nextSlide}>
               <Angleright />
             </SliderButton>
           </div>
         </div>
 
-
         <nav
           className={`
-    fixed top-0 left-0 w-full h-20
-    gap-8
-    flex items-center justify-between
-    px-6 md:px-12
-  
-
-    ${openMenu ? "bg-white" : "bg-transparent"}
-    md:bg-transparent`}
+            fixed top-0 left-0 w-full h-20 2xl:h-32 
+            gap-8 flex items-center justify-between
+            px-6 md:px-12 2xl:px-24 2xl:pt-12
+            z-50
+            ${openMenu ? "bg-white" : "bg-transparent"}
+            md:bg-transparent
+          `}
         >
-          {/* Left: close / hamburger */}
           <button
             className="md:hidden text-2xl"
             onClick={() => setOpenMenu(!openMenu)}
           >
-            <img src={openMenu ? closeIcon : hamburgerIcon} alt="menu" className="cursor-pointer" />
-
+            <img
+              src={openMenu ? closeIcon : hamburgerIcon}
+              alt="menu"
+              className="cursor-pointer"
+            />
           </button>
 
-
-          {/* Center logo */}
           <img
             src={logo}
             alt="Room logo"
-            className={`mx-auto md:mx-0 transition-all duration-300
-    ${openMenu ? "hidden" : "block"} md:block
-  `}
+            className={`
+              mx-auto md:mx-0 transition-all duration-300
+              2xl:scale-125 2xl:origin-left
+              ${openMenu ? "hidden" : "block"} md:block
+            `}
           />
 
-
-          {/* Links */}
           <div
             className={`
-     w-full
-    flex  gap-8 font-semibold
-    bg-white py-6
-    md:static md:bg-transparent md:py-0
-    ${openMenu ? "flex" : "hidden"}
-    md:flex md:items-center 
-  `}
+              w-full flex gap-8 font-semibold
+              bg-white py-6 md:static md:bg-transparent md:py-0
+              ${openMenu ? "flex" : "hidden"}
+              md:flex md:items-center 
+              2xl:gap-12 
+            `}
           >
-
             {["home", "shop", "about", "contact"].map((item) => (
               <a
                 key={item}
                 href={`#${item}`}
                 className="
-          text-black md:text-white
-          lowercase text-sm font-semibold
-          relative
-          after:content-['']
-          after:absolute after:left-0 after:-bottom-1.5
-          after:w-full after:h-0.5 after:bg-current
-          after:scale-x-0 after:transition-transform
-          hover:after:scale-x-100
-        "
+                  text-black md:text-white
+                  lowercase text-sm font-semibold
+                  2xl:text-lg
+                  relative
+                  after:content-['']
+                  after:absolute after:left-0 after:-bottom-1.5
+                  after:w-full after:h-0.5 after:bg-current
+                  after:scale-x-0 after:transition-transform
+                  hover:after:scale-x-100
+                "
               >
                 {item}
               </a>
             ))}
           </div>
         </nav>
-
       </div>
 
-
-      <div className="w-full md:relative md:w-[42%] flex flex-col justify-center p-10 md:px-20 gap-5">
-
-        <h1 className="text-3xl md:text-4xl font-bold leading-tight">
+      <div className="w-full md:relative md:w-[42%] flex flex-col justify-center p-10 md:px-20 2xl:px-32 gap-5 2xl:gap-10">
+        <h1 className="text-3xl md:text-4xl 2xl:text-5xl font-bold leading-tight 2xl:leading-tight">
           {activeSlide.title}
         </h1>
-        <p className="text-(--grey-500) text-[12px] leading-4 font-medium mb-5">
+
+        <p className="text-gray-500 text-[12px] 2xl:text-base leading-4 2xl:leading-7 font-medium mb-5 2xl:mb-8">
           {activeSlide.text}
         </p>
 
-
-        <button className="flex items-center gap-6 text-black uppercase tracking-[0.8em] text-sm font-bold hover:text-(--grey-500) cursor-pointer transition-colors duration-300">
+        <button className="flex items-center gap-6 2xl:gap-10 text-black uppercase tracking-[0.8em] 2xl:tracking-[1em] text-sm 2xl:text-xl font-bold hover:text-gray-500 cursor-pointer transition-colors duration-300">
           Shop Now
           <span>
             <Iconarrow />
@@ -204,7 +199,6 @@ function Homepage() {
 
         <div className="hidden md:absolute md:block bottom-0 left-0">
           <div className="flex">
-
             <SliderButton onClick={prevSlide}>
               <Angleleft />
             </SliderButton>
@@ -213,12 +207,9 @@ function Homepage() {
             </SliderButton>
           </div>
         </div>
-
       </div>
     </div>
   );
 }
 
 export default Homepage;
-
-
